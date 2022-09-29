bdf70c48a71b986bdb94b4c2423fb48646813e81
Hongsi-Taste <94719411+Hongsi-Taste@users.noreply.github.com>
2022-09-30 01:18:26 +0900

    main screen added bottom navigation bar fragment change

--- app/src/main/java/gachon/teama/frimo/MainActivity.kt
+++ app/src/main/java/gachon/teama/frimo/MainActivity.kt
@@ -3,7 +3,12 @@ package gachon.teama.frimo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import gachon.teama.frimo.databinding.ActivityMainBinding
+
+private const val TAG_CHAT = "chatting"
+private const val TAG_DIARY = "diary"
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.icChat.setOnClickListener {
-            startActivity(Intent(this, ChattingActivity::class.java))
+        setFragment(TAG_CHAT, chatting())
+
+        binding.naviView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.chatFragment -> setFragment(TAG_CHAT, chatting())
+                R.id.diaryFragment -> setFragment(TAG_DIARY, diary())
+            }
+            true
         }
 
+
+//        binding.icChat.setOnClickListener {
+//            startActivity(Intent(this, ChattingActivity::class.java))
+//        }
+
+    }
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragmentTransaction = manager.beginTransaction()
+
+        if(manager.findFragmentByTag(tag)==null){
+            fragmentTransaction.add(R.id.mainFrameLayout, fragment, tag)
+        }
+
+        val chat = manager.findFragmentByTag(TAG_CHAT)
+        val diary = manager.findFragmentByTag(TAG_DIARY)
+
+        //Transaction.hide
+        if(chat != null){
+            fragmentTransaction.hide(chat)
+        }
+        if (diary != null){
+            fragmentTransaction.hide(diary)
+        }
+
+        //Transaction.show
+        if(tag== TAG_CHAT){
+            if(chat!=null){
+                fragmentTransaction.show(chat)
+            }
+        }
+        else if(tag== TAG_DIARY){
+            if(diary!=null){
+                fragmentTransaction.show(diary)
+            }
+        }
+
+        fragmentTransaction.commitAllowingStateLoss()
     }
 }

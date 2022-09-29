b19f92114b3a0d96ba2d043844d20bf7513d9284
Hongsi-Taste <94719411+Hongsi-Taste@users.noreply.github.com>
2022-09-30 01:15:13 +0900

    loading activity frame

---
+++ app/src/main/java/gachon/teama/frimo/Loading.kt
@@ -0,0 +1,11 @@
+package gachon.teama.frimo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Loading : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}

6c84343dd501abde8b788c5e66d8fe209f4617bf
namsh1125 <namsh1125@naver.com>
2022-11-10 09:05:51 +0900

    Modify character entity

--- app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
+++ app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
@@ -5,14 +5,17 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Character (
-    var name: String, // 이름
-    var image: Int, // 캐릭터 이미지
+    var name: String, // Character name
+    var image: Int, // Character image
     var like: Boolean, // 캐릭터 하트 여부
-    var introduce : String, // 캐릭터 소개
-    var live : String, // 캐릭터 사는 곳
+    var introduce : String, // Introduce character
+    var live : String, // Where character lives
+    var img_live : Int, // (Image) Where character lives
     var height : String, // 캐릭터 키
-    var preference_image : Int, // 캐릭터가 선호하는 것 (이미지)
-    var preference : String // 캐릭터가 선호하는 것 (String)
+    var prefer : String, // What character prefers
+    var img_prefer : Int, // (Image) What character prefers
+    var img_recommendation : Int, // 오늘의 추천 캐릭터에서 보여줄 이미지
+    var img_theme : Int // 캐릭터 테마에서 보여줄 이미지
 ) {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 }

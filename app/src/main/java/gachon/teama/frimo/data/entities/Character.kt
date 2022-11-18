611fbbaa780c083dd49a8d76d78721d13658454d
namsh1125 <namsh1125@naver.com>
2022-11-18 19:17:46 +0900

    Add character live, prefer image

--- app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
+++ app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
@@ -6,12 +6,12 @@ import androidx.room.PrimaryKey
 @Entity
 data class Character (
     var name: String, // Character name
-    var image: Int, // Character image
+    var img_profile: Int, // Character image
     var like: Boolean, // 캐릭터 하트 여부
     var introduce : String, // Introduce character
     var live : String, // Where character lives
     var img_live : Int, // (Image) Where character lives
-    var height : String, // 캐릭터 키
+    var img_height : String, // 캐릭터 키
     var prefer : String, // What character prefers
     var img_prefer : Int, // (Image) What character prefers
     var img_recommendation : Int, // 오늘의 추천 캐릭터에서 보여줄 이미지

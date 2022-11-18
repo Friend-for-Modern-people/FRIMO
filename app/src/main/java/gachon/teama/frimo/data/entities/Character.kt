9a9b3da63b21e0c175ec174ad65112393c1e99ca
namsh1125 <namsh1125@naver.com>
2022-11-18 20:03:17 +0900

    Add character recommendation image

--- app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
+++ app/src/main/java/gachon/teama/frimo/data/entities/Character.kt
@@ -11,7 +11,8 @@ data class Character (
     var introduce : String, // Introduce character
     var live : String, // Where character lives
     var img_live : Int, // (Image) Where character lives
-    var img_height : String, // 캐릭터 키
+    var height : String, // 캐릭터 키
+    var img_height : String, // (Image) 캐릭터 키
     var prefer : String, // What character prefers
     var img_prefer : Int, // (Image) What character prefers
     var img_recommendation : Int, // 오늘의 추천 캐릭터에서 보여줄 이미지

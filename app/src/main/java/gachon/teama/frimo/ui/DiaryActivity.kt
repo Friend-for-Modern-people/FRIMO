048452fece774c8549635aca1b34308facdbd87c
namsh1125 <namsh1125@naver.com>
2022-11-23 20:09:46 +0900

    (DiaryFragment) when diary clicked show diary information

---
+++ app/src/main/java/gachon/teama/frimo/ui/DiaryActivity.kt
@@ -0,0 +1,13 @@
+package gachon.teama.frimo.ui
+
+import gachon.teama.frimo.base.BaseActivity
+import gachon.teama.frimo.databinding.ActivityDiaryBinding
+
+class DiaryActivity : BaseActivity<ActivityDiaryBinding>(ActivityDiaryBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+
+    }
+
+}
--- app/src/main/java/gachon/teama/frimo/ui/FilteredDiaryFragment.kt
+++ app/src/main/java/gachon/teama/frimo/ui/FilteredDiaryFragment.kt
@@ -1,15 +1,60 @@
 package gachon.teama.frimo.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import gachon.teama.frimo.R
+import gachon.teama.frimo.databinding.FragmentFilteredDiaryBinding
 
-class FilteredDiaryFragment: Fragment() {
+class FilteredDiaryFragment : Fragment() {
+
+    // Binding
+    private lateinit var binding: FragmentFilteredDiaryBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_filtered_diary, container, false)
+
+        binding = FragmentFilteredDiaryBinding.inflate(layoutInflater)
+
+        setClickListener()
+
+        return binding.root
+    }
+
+    private fun setClickListener() {
+
+        binding.layoutFilter1Detail.setOnClickListener {
+
+        }
+
+        binding.layoutFilter2Detail.setOnClickListener {
+
+        }
+
+        // When filter1 diary1 clicked
+        binding.filter1Diary1.setOnClickListener {
+            // Todo: Intent로 diary 넣어 전달
+            startActivity(Intent(requireContext(), DiaryActivity::class.java))
+        }
+
+        // When filter1 diary2 clicked
+        binding.filter1Diary2.setOnClickListener {
+            // Todo: Intent로 diary 넣어 전달
+            startActivity(Intent(requireContext(), DiaryActivity::class.java))
+        }
+
+        // When filter2 diary1 clicked
+        binding.filter2Diary1.setOnClickListener {
+            // Todo: Intent로 diary 넣어 전달
+            startActivity(Intent(requireContext(), DiaryActivity::class.java))
+        }
+
+        // When filter2 diary2 clicked
+        binding.filter2Diary2.setOnClickListener {
+            // Todo: Intent로 diary 넣어 전달
+            startActivity(Intent(requireContext(), DiaryActivity::class.java))
+        }
+
     }
 }

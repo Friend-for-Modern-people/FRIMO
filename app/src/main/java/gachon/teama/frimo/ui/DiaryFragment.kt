25149ca587673cf64feb875f3b1f1d8fdb9385cc
namsh1125 <namsh1125@naver.com>
2022-11-28 09:54:52 +0900

    Show filter popup

--- app/src/main/java/gachon/teama/frimo/ui/DiaryFragment.kt
+++ app/src/main/java/gachon/teama/frimo/ui/DiaryFragment.kt
@@ -1,9 +1,13 @@
 package gachon.teama.frimo.ui
 
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
 import androidx.fragment.app.Fragment
 import gachon.teama.frimo.R
 import gachon.teama.frimo.data.local.AppDatabase
@@ -36,8 +40,42 @@ class DiaryFragment : Fragment(){
         // 최초 실행시 보이는 fragment
         childFragmentManager.beginTransaction().replace(R.id.frame, FilteredDiaryFragment()).commit()
 
+        setClickListener()
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun setClickListener(){
+
+        binding.buttonFilter.setOnClickListener {
+
+            // 클릭시 팝업 윈도우 생성
+            val popupWindow = PopupWindow(it)
+            val inflater = context?.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+            // 팝업으로 띄울 커스텀뷰를 설정하고
+            val view: View = inflater.inflate(R.layout.view_popup_sort, null)
+            popupWindow.contentView = view
+
+            // popup window 크기 설정
+            popupWindow.setWindowLayoutMode(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+            // popup window 터치 되도록
+            popupWindow.isTouchable = true
+
+            // 포커스
+            popupWindow.isFocusable = true
+
+            // popup window 이외에도 터치되게 (터치시 팝업 닫기 위한 코드)
+            popupWindow.isOutsideTouchable = true
+            popupWindow.setBackgroundDrawable(BitmapDrawable())
+
+            // popup window 보여주기
+            popupWindow.showAtLocation(it, Gravity.BOTTOM, 0, 0)
+
+        }
+
     }
 
     private fun getDiaryFromServer() {

bc017e07364cf040dffafc4f748d50877ffa4e3a
namsh1125 <namsh1125@naver.com>
2022-11-30 01:32:37 +0900

    Show message when user didn't check

--- app/src/main/java/gachon/teama/frimo/ui/AuthorityActivity.kt
+++ app/src/main/java/gachon/teama/frimo/ui/AuthorityActivity.kt
@@ -3,16 +3,14 @@ package gachon.teama.frimo.ui
 
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
+import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import gachon.teama.frimo.base.BaseActivity
 import gachon.teama.frimo.databinding.ActivityAuthorityBinding
 
-class AuthorityActivity : ComponentActivity() {
-
-    private lateinit var binding: ActivityAuthorityBinding
+class AuthorityActivity : BaseActivity<ActivityAuthorityBinding>(ActivityAuthorityBinding::inflate) {
 
     // 퍼미션 응답 처리 코드
     private val multiplePermissionsCode = 100
@@ -26,12 +24,11 @@ class AuthorityActivity : ComponentActivity() {
         android.Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun initAfterBinding() {
+        setClickListener()
+    }
 
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityAuthorityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    private fun setClickListener() {
 
         with(binding) {
 
@@ -47,30 +44,69 @@ class AuthorityActivity : ComponentActivity() {
                     checkboxInternet.isChecked = true
                     checkboxCamera.isChecked = true
                     checkboxMic.isChecked = true
+                    textviewTextGiveAuthority1.visibility = View.GONE
+                    textviewTextGiveAuthority2.visibility = View.GONE
+                    textviewTextGiveAuthority3.visibility = View.GONE
+                    textviewTextGiveAuthority4.visibility = View.GONE
+
+
                 } else {
 
                     checkboxFile.isChecked = false
                     checkboxInternet.isChecked = false
                     checkboxCamera.isChecked = false
                     checkboxMic.isChecked = false
+                    textviewTextGiveAuthority1.visibility = View.VISIBLE
+                    textviewTextGiveAuthority2.visibility = View.VISIBLE
+                    textviewTextGiveAuthority3.visibility = View.VISIBLE
+                    textviewTextGiveAuthority4.visibility = View.VISIBLE
+
                 }
 
             }
 
             checkboxFile.setOnClickListener {
                 updateCheckbox()
+
+                if (checkboxFile.isChecked) {
+                    textviewTextGiveAuthority1.visibility = View.GONE
+                } else {
+                    textviewTextGiveAuthority1.visibility = View.VISIBLE
+                }
+
             }
 
             checkboxInternet.setOnClickListener {
                 updateCheckbox()
+
+                if (checkboxInternet.isChecked) {
+                    textviewTextGiveAuthority2.visibility = View.GONE
+                } else {
+                    textviewTextGiveAuthority2.visibility = View.VISIBLE
+                }
+
             }
 
             checkboxCamera.setOnClickListener {
                 updateCheckbox()
+
+                if (checkboxCamera.isChecked) {
+                    textviewTextGiveAuthority3.visibility = View.GONE
+                } else {
+                    textviewTextGiveAuthority3.visibility = View.VISIBLE
+                }
+
             }
 
             checkboxMic.setOnClickListener {
                 updateCheckbox()
+
+                if (checkboxMic.isChecked) {
+                    textviewTextGiveAuthority4.visibility = View.GONE
+                } else {
+                    textviewTextGiveAuthority4.visibility = View.VISIBLE
+                }
+
             }
 
         }
@@ -83,9 +119,9 @@ class AuthorityActivity : ComponentActivity() {
 
     }
 
-    fun updateCheckbox(){
+    private fun updateCheckbox() {
 
-        with(binding){
+        with(binding) {
 
             // If any of the lower checkboxes are not clicked, unclick the upper checkbox.
             // When all child checkboxes are clicked, the parent checkbox is clicked
@@ -96,15 +132,15 @@ class AuthorityActivity : ComponentActivity() {
         }
     }
 
-//    // 퍼미션 체크 및 권한 요청 함수
+    // 퍼미션 체크 및 권한 요청 함수
     private fun checkPermissionsAndRun() {
 
         // 거절되었거나 아직 수락하지 않은 권한(퍼미션)을 저장할 문자열 배열 리스트
         val rejectedPermissionList = ArrayList<String>()
 
         // 필요한 퍼미션들을 하나씩 끄집어내서 현재 권한을 받았는지 체크
-        for(permission in requiredPermissions){
-            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        for (permission in requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
 
                 // 만약 권한이 없다면 rejectedPermissionList에 추가
                 rejectedPermissionList.add(permission)
@@ -112,7 +148,7 @@ class AuthorityActivity : ComponentActivity() {
         }
 
         // 거절된 퍼미션이 있다면...
-        if(rejectedPermissionList.isNotEmpty()){
+        if (rejectedPermissionList.isNotEmpty()) {
 
             // 권한 요청!
             val array = arrayOfNulls<String>(rejectedPermissionList.size)
@@ -121,16 +157,15 @@ class AuthorityActivity : ComponentActivity() {
     }
 
     // 권한 요청 결과 함수
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         // Todo: 권한을 전부 못 받은 경우, 권한을 전부 허락해줄 때까지 요청 보내기
         when (requestCode) {
             multiplePermissionsCode -> {
-                if(grantResults.isNotEmpty()) {
-                    for((i, permission) in permissions.withIndex()) {
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.isNotEmpty()) {
+                    for ((i, permission) in permissions.withIndex()) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             // 권한 획득 실패
                             Log.i("TAG", "The user has denied to $permission")
                         }
